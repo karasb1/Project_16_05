5fa119677872af8d54c1eb0f1ffb818e126f1ecc
karasb1 <bohdankaras456@gmail.com>
2024-05-16 19:16:27 +0300

    The ex3 was done

--- main.cpp
+++ main.cpp
@@ -3,7 +3,41 @@ using namespace std;
 
 int main()
 {
-
+	int col, row;
+	cout << "Enter row: ";
+	cin >> row;
+	cout << "Enter col: ";
+	cin >> col;
+	if (row % 2 == 0) {
+		for (size_t i = 0; i < (row / 2); i++)
+		{
+			for (size_t j = 0; j < col; j++)
+			{
+				if (j >= i && i+j < col) {
+					cout << "*" << " ";
+				}
+				else {
+					cout << "  ";
+				}
+			}
+			cout << endl;
+		}
+	}
+	if (row % 2 != 0) {
+		for (size_t i = 0; i < (row / 2 + 1); i++)
+		{
+			for (size_t j = 0; j < col; j++)
+			{
+				if (j >= i && i + j < col) {
+					cout << "*" << " ";
+				}
+				else {
+					cout << "  ";
+				}
+			}
+			cout << endl;
+		}
+	}
 
 	return 0;
 }
