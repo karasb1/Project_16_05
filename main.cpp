c549cd79f06c31a596043e0977a4e332c1a5ec1c
karasb1 <bohdankaras456@gmail.com>
2024-05-16 20:16:08 +0300

    The ex1 was done.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,30 @@ using namespace std;
 
 int main()
 {
-
+	int a, b, c, d;
+	cout << "Enter first number of range: ";
+	cin >> a;
+	cout << "Enter second number of range: ";
+	cin >> b;
+	if (a > b)
+	{
+		d = a;
+		a = b;
+		b = d;
+	}
+	while (true)
+	{
+		cout << "Enter number: ";
+		cin >> c;
+		if (c >= a && c <= b)
+		{
+			break;
+		}
+		else
+		{
+			continue;
+		}
+	}
 
 	return 0;
 }
