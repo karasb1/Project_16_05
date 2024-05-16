6dad94bcb186e7704ccece2b593e1137d48760ce
karasb1 <bohdankaras456@gmail.com>
2024-05-16 20:04:58 +0300

    The ex5 was done.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,70 @@ using namespace std;
 
 int main()
 {
+	int col, row;
+	cout << "Enter row: ";
+	cin >> row;
+	cout << "Enter col: ";
+	cin >> col;
+	if (row % 2 == 0) {
+		for (size_t i = 0; i < (row / 2); i++)
+		{
+			for (size_t j = 0; j < col; j++)
+			{
+				if (j >= i && i + j < col) {
+					cout << "*" << " ";
+				}
+				else {
+					cout << "  ";
+				}
+			}
+			cout << endl;
+		}
 
+		for (size_t i = row / 2; i < row; i++)
+		{
+			for (size_t j = 0; j < col; j++)
+			{
+				if (i >= j && i + j >= col - 1) {
+					cout << "*" << " ";
+				}
+				else {
+					cout << "  ";
+				}
+			}
+			cout << endl;
+		}
+
+	}
+	if (row % 2 != 0) {
+		for (size_t i = 0; i < (row / 2 + 1); i++)
+		{
+			for (size_t j = 0; j < col; j++)
+			{
+				if (j >= i && i + j < col) {
+					cout << "*" << " ";
+				}
+				else {
+					cout << "  ";
+				}
+			}
+			cout << endl;
+		}
+
+		for (size_t i = row / 2; i < row; i++)
+		{
+			for (size_t j = 0; j < col; j++)
+			{
+				if (i >= j && i + j >= row - 1) {
+					cout << "*" << " ";
+				}
+				else {
+					cout << "  ";
+				}
+			}
+			cout << endl;
+		}
+	}
 
 	return 0;
 }
