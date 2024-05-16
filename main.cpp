c16641ddc70481c7c5b5a9051305710de8d91b89
karasb1 <bohdankaras456@gmail.com>
2024-05-16 19:01:44 +0300

    The ex2 was done

--- main.cpp
+++ main.cpp
@@ -3,7 +3,19 @@ using namespace std;
 
 int main()
 {
-
+	for (size_t i = 0; i < 5; i++)
+	{
+		for (size_t j = 0; j < 5; j++)
+		{
+			if (j <= i) {
+				cout << "*" << " ";
+			}
+			else {
+				cout << "  ";
+			}
+		}
+		cout << endl;
+	}
 
 	return 0;
 }
